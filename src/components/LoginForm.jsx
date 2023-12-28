cc46e07d86da45b4c517e17f1b84adec7aeb4740
Haider Ali <hassanhussaian630@gmail.com>
2023-12-28 11:27:19 +0500

    commit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,52 +1,60 @@
 "use client";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
 
 const LoginForm = ({ focusEmailInput, buttontitle, forgotpassword }) => {
   const emailRef = useRef(null);
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
   useEffect(() => {
     if (focusEmailInput && emailRef.current) {
       emailRef.current.focus();
     }
   }, [focusEmailInput]);
-  function showPassword() {
-    const passInput = document.getElementById("input_password");
-    const passShow = document.getElementById('show_password')
-    if (passInput.type === "password") {
-      passInput.type = "text";
-      passShow.innerHTML == 'Hide'
-    } else {
-      passInput.type = "password";
-      passShow.innerHTML = 'Show'
-    }
+
+  function togglePasswordVisibility() {
+    setPasswordVisible((prev) => !prev);
   }
+
+  const passwordInputType = passwordVisible ? "text" : "password";
+  const showPasswordText = passwordVisible ? "Hide" : "Show";
+
   return (
     <div className="ml-8 mt-14">
       <form className="flex flex-col space-y-3 w-[25rem]">
+        <label htmlFor="email">Email or Phone Number</label>
         <input
           type="email"
+          id="email"
           placeholder="Email or Phone Number"
           className="border-2 border-black/25 rounded-sm p-3 outline-none"
           ref={emailRef}
         />
+
+        <label htmlFor="password">Password</label>
         <input
-          type="password"
+          type={passwordInputType}
           placeholder="Password"
           className="border-2 border-black/25 rounded-sm p-3 outline-none"
-          id="input_password"
+          id="password"
         />
-        <p className="cursor-pointer" id="show_password" onClick={showPassword}>Show</p>
+
+        <p className="cursor-pointer  relative left-[21rem] -top-12 " onClick={togglePasswordVisibility}>
+          {showPasswordText}
+        </p>
+
         <span className="text-blue-400 cursor-pointer hover:underline">
           {forgotpassword}
         </span>
+
         <Button
           variant="outline"
           className="bg-blue-500 text-white border-2 border-blue-500"
         >
           {buttontitle}
         </Button>
+
         <span>
           Don&rsquo;t have an account?{" "}
           <Link
